ecb227b1e705eaf3c0fa72592a2134aa3e10ad14
alishakavene <akmunywoki@gmail.com>
2020-07-24 07:19:05 +0300

    delete pitch2 and add config file

--- app/config.py
+++ app/config.py
@@ -1,5 +1,15 @@
-pitch_data = {
-    'question' : 'Which successful quote, do you like?'
-    ' pitches' : ['The best revenge is success', 'success is just a journey not a destination','You get what you focus on',] 
+class Config:
+    
+    pass
 
-}
+
+
+class ProdConfig(Config):
+   
+    pass
+
+
+class DevConfig(Config):
+    
+
+    DEBUG = True
---
+++ app/models.py
@@ -0,0 +1,7 @@
+class Data:
+    
+pitch_data = {
+    'question' : 'Which successful quote, do you like?'
+    ' pitches' : ['The best revenge is success', 'success is just a journey not a destination','You get what you focus on',] 
+
+}
