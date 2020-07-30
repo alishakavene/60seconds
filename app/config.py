33008e77e568c880215234e051e3b60114771db4
alishakavene <akmunywoki@gmail.com>
2020-07-30 08:35:14 +0300

    add README

--- app/config.py
+++ app/config.py
@@ -3,6 +3,9 @@ import os
 class Config:
     
     SECRET_KEY=os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
+    print (SQLALCHEMY_DATABASE_URI, 'STRING')
+
 
 
 
