84e19c05df180f9ba3b64a7c42345c968a432ca0
alishakavene <akmunywoki@gmail.com>
2020-08-03 07:38:19 +0300

    import request to main/views file

--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,16 @@ from flask import render_template,request,redirect,url_for
 from . import main
 
 
+
+
 @main.route('/')
 def root():
      title = 'Home - Welcome to 60seconds pitch'
      return render_template('index.html', title =title)
+
+#@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+#@login_required
+#def new_review(id):
 
 
 
